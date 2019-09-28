a9762c1adc4fdcba47bc7862107e2eeb7775fb51
zhihy <zhihaoyu@pt.picc.com.cn>
2019-09-28 15:34:09 +0800

    删除无用引用 补充注释 调整方法顺序

--- src/main/java/com/datadiff/util/ExcelImportUtil.java
+++ src/main/java/com/datadiff/util/ExcelImportUtil.java
@@ -3,19 +3,19 @@ package com.datadiff.util;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.text.DateFormat;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -23,7 +23,6 @@ import java.util.List;
 
 /**
  * excel导入工具
- * // todo 补充注释
  *
  * @author zhihaoyu
  */
@@ -34,9 +33,38 @@ public class ExcelImportUtil {
     private static final String EXCEL_2007_SUFFIX = ".xlsx";
     private static final String DEFAULT_DATE_PATTERN = "yyyy-MM-dd";
 
+    /**
+     * 从本地读取excel并获取集合
+     *
+     * @param path        本地excel存放路径
+     * @param headerProps 与导入模板的表格表头对应的属性名数组（顺序一致，一一对应）
+     * @param targetClass 要将表格数据封装成的目标类信息
+     * @return 集合
+     */
     public static <T> List<T> getListFromExcel(String path, String[] headerProps,
                                                Class<T> targetClass) {
         return getListFromExcel(path, headerProps, targetClass, DEFAULT_DATE_PATTERN);
+    }
+
+    /**
+     * 从本地读取excel并获取集合
+     * 注：此方法可以指定日期格式
+     *
+     * @param path        本地excel存放路径
+     * @param headerProps 与导入模板的表格表头对应的属性名数组（顺序一致，一一对应）
+     * @param targetClass 要将表格数据封装成的目标类信息
+     * @param datePattern 日期格式
+     * @return 集合
+     */
+    public static <T> List<T> getListFromExcel(String path, String[] headerProps,
+                                               Class<T> targetClass, String datePattern) {
+        List<T> list = new ArrayList<>();
+        try (Workbook workbook = getWorkBookByPath(path)) {
+            list = getListFromWorkBook(workbook, headerProps, targetClass, datePattern);
+        } catch (Exception e) {
+            log.error("Error occurred when getting list from excel!", e);
+        }
+        return list;
     }
 
     /**
@@ -45,6 +73,7 @@ public class ExcelImportUtil {
      * @param excel       待导入excel对象
      * @param headerProps 与导入模板的表格表头对应的属性名数组（顺序一致，一一对应）
      * @param targetClass 要将表格数据封装成的目标类信息
+     * @return 集合
      */
     public static <T> List<T> getListFromExcel(MultipartFile excel, String[] headerProps,
                                                Class<T> targetClass) {
@@ -71,22 +100,12 @@ public class ExcelImportUtil {
         return list;
     }
 
-    public static <T> List<T> getListFromExcel(String path, String[] headerProps,
-                                               Class<T> targetClass, String datePattern) {
-        List<T> list = new ArrayList<>();
-        try (Workbook workbook = getWorkBookByPath(path)) {
-            list = getListFromWorkBook(workbook, headerProps, targetClass, datePattern);
-        } catch (Exception e) {
-            log.error("Error occurred when getting list from excel!", e);
-        }
-        return list;
-    }
-
     /**
      * 根据要导入的excel的格式获取相应的workbook
      *
      * @param excel 要导入的excel
      * @return 格式不正确时返回null
+     * @throws IOException 输入输出异常
      */
     private static Workbook getWorkBookByExcel(MultipartFile excel) throws IOException {
         String suffix = getSuffixByExcel(excel);
@@ -94,12 +113,27 @@ public class ExcelImportUtil {
         return getWorkBook(suffix, inputStream);
     }
 
-    private static Workbook getWorkBookByPath(String path) throws Exception {
+    /**
+     * 从本地读取excel并根据其格式获取相应的workbook
+     *
+     * @param path 本地excel存放路径
+     * @return 格式不正确时返回null
+     * @throws IOException 输入输出异常
+     */
+    private static Workbook getWorkBookByPath(String path) throws IOException {
         String suffix = getSuffix(path);
         InputStream inputStream = new FileInputStream(path);
         return getWorkBook(suffix, inputStream);
     }
 
+    /**
+     * 根据后缀和输入流获取workbook
+     *
+     * @param suffix 后缀
+     * @param ins    输入流
+     * @return workbook
+     * @throws IOException 输入输出异常
+     */
     private static Workbook getWorkBook(String suffix, InputStream ins) throws IOException {
         if (EXCEL_2003_SUFFIX.equals(suffix)) {
             return new HSSFWorkbook(ins);
@@ -111,6 +145,14 @@ public class ExcelImportUtil {
         return null;
     }
 
+    /**
+     * 从工作簿中获取列表
+     *
+     * @param workbook    工作部
+     * @param headerProps 与导入模板的表格表头对应的属性名数组（顺序一致，一一对应）
+     * @param targetClass 要将表格数据封装成的目标类信息
+     * @return 列表
+     */
     private static <T> List<T> getListFromWorkBook(Workbook workbook, String[] headerProps,
                                                    Class<T> targetClass, String datePattern) throws Exception {
         List<T> list = new ArrayList<>();
@@ -155,6 +197,12 @@ public class ExcelImportUtil {
         return getSuffix(filename);
     }
 
+    /**
+     * 根据默认分隔符"."获取字符串的后缀
+     *
+     * @param str 字符串
+     * @return 后缀
+     */
     private static String getSuffix(String str) {
         String period = ".";
         if (StringUtils.isBlank(str) || !str.contains(period)) {
